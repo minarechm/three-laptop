c15a34f9e2f73efcea6ad23b717227411b71883a
minarechm <minarechma@gmail.com>
2023-01-01 02:26:09 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { Canvas } from "@react-three/fiber";
+import { Html, ContactShadows, useGLTF, OrbitControls, Environment, Float, Center} from "@react-three/drei"
+
+const Scene = () => {
+  const model = useGLTF("./model.gltf")
+  return (
+    <>
+    <Float>
+      <Center> 
+        <Environment preset="city"/>
+        <color args={["#2596be"]} attach="background"/>
+        <OrbitControls
+          minPolarAngle={Math.PI / 2.2} 
+          maxPolarAngle={Math.PI / 2.5}
+          minAzimuthAngle={Math.PI / 54.7}
+          maxAzimuthAngle={Math.PI / 4.3}
+        />
+        <rectAreaLight
+          width={2.5}
+          height={1.65}
+          intensity={65}
+          color={"#ff6900"}
+          rotation={ [0.1, Math.PI, 0]}
+          position={ [0, 0.55, -1.15]}
+        />
+        <primitive object={model.scene}>
+          <Html
+            transform
+            wrapperClass="htmlScreen"
+            distanceFactor={1.17}
+            position={ [0, 1.56, -1.4]}
+            rotation-x={ - 0.256}
+          >
+            <iframe src="http://test.lmi.sk/"></iframe>
+          </Html>
+        </primitive>
+      </Center>
+    </Float>
+    <ContactShadows 
+      position-y={ - 1.4 }
+      opacity={0.4}
+      blur={1}
+      scale={5}
+    />
+    </>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas>
+      <Scene/>
+    </Canvas>
   );
 }
 
